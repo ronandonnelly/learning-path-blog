22000bf0dc6cdf71adc28bea5f8db1b97645b81e
Ronan Donnelly <ronan.donnelly@teamfeepay.com>
2023-10-19 15:12:59 +0100

    tests updated

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,6 +87,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.require_master_key = true
+
 
 
   # Enable DNS rebinding protection and other `Host` header attacks.
