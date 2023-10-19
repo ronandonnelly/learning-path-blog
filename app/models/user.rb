5471e62f9263c013feaa3b31c1d96d8b8c6dd821
Ronan Donnelly <ronan.donnelly@teamfeepay.com>
2023-10-19 14:30:08 +0100

    tests written

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :recoverable, :rememberable, :validatable
 end
 # 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
