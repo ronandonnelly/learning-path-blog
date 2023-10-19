692b34877402cea03b8bfca93075035b087bf69f
Ronan Donnelly <ronan.donnelly@teamfeepay.com>
2023-10-19 02:37:08 +0100

    credentials1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,6 +87,12 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  Rails.application.configure do
+    ...
+      config.secret_key_base = ENV["SECRET_KEY_BASE"]
+      ...
+  end
+
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
