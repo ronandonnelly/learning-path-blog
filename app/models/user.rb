3b51de3864ac6d399bb111131cb2c870e224fc0d
Ronan Donnelly <ronan.donnelly@teamfeepay.com>
2023-10-19 02:31:34 +0100

    credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,3 +6,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :recoverable, :rememberable, :validatable
 end
+# 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
