2ef319ebb7214cf254be1714180951c094091e46
ronandonnelly <ronan.donnelly@teamfeepay.com>
2023-10-25 11:55:55 +0100

    Finished product... sorta

--- app/controllers/blog_posts/cover_images_controller.rb
+++ app/controllers/blog_posts/cover_images_controller.rb
@@ -1,15 +1,21 @@
-class BlogPosts::CoverImages < ApplicationController
+class BlogPosts::CoverImagesController < ApplicationController
+    include ActionView::RecordIdentifier
+  
     before_action :authenticate_user!
     before_action :set_blog_post
-
+  
     def destroy
       @blog_post.cover_image.purge_later
-      redirect_to edit_blog_post_path(@blog_post)
+      
+      respond_to do |format| 
+        format.html { redirect_to edit_blog_post_path(@blog_post) }
+        format.turbo_stream { render turbo_stream: turbo_stream.remove(dom_id(@blog_post, :cover_image)) }
+      end
     end
-    
+  
     private
-
+  
     def set_blog_post
-      @blog_post = BlogPost.find(params[:blog_post_id])  
+      @blog_post = BlogPost.find(params[:blog_post_id])
     end
-end
+  end
