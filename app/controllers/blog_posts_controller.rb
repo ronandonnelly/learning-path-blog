a5f17c05488992a9e7980b6600e072009bc6e570
ronandonnelly <ronan.donnelly@teamfeepay.com>
2023-10-25 11:38:56 +0100

    Cover image testin

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -5,7 +5,8 @@ class BlogPostsController < ApplicationController
   def index
     @blog_posts = user_signed_in? ? BlogPost.sorted : BlogPost.published.sorted
     @pagy, @blog_posts = pagy(@blog_posts)
-    
+  rescue Pagy::OverflowError 
+    redirect_to root_path(page: 1)
   end
 
   def show
@@ -43,7 +44,7 @@ class BlogPostsController < ApplicationController
   private
 
   def blog_post_params
-    params.require(:blog_post).permit(:title, :content, :published_at)
+    params.require(:blog_post).permit(:title, :content, :cover_image, :published_at)
   end
 
   def set_blog_post
