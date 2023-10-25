581ff8a3ef467b4ff21936962d844917adb92618
ronandonnelly <ronan.donnelly@teamfeepay.com>
2023-10-25 11:34:03 +0100

    Cover image test

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users #path: 'users', controllers: { registrations: 'users/registrations' }
-  resources :blog_posts
+  
+  devise_for :users
+  
+  resources :blog_posts do
+    resource :cover_image, only: [:destroy], module: :blog_posts
+  end
+  
   root "blog_posts#index"
+  # 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
 end
